b5e6009e3e581e0a8a1ae195439f1de0371f356d
theozebua <ronizebua.rz@gmail.com>
2023-02-02 13:17:14 +0700

    `refactor:` create css component for button

--- react-frontend/src/pages/auth/SignUp.tsx
+++ react-frontend/src/pages/auth/SignUp.tsx
@@ -139,7 +139,7 @@ export default (): JSX.Element => {
         </div>
         <div className='mt-4'>
           <button
-            className='block w-full rounded-lg bg-gray-900 px-4 py-2 font-semibold text-gray-100'
+            className='btn btn-dark w-full'
             form='signup-form'>
             Sign Up
           </button>
