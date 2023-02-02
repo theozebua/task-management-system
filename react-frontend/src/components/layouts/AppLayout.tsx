7e8c0a25f799ed6309b34c85a6f45c7e55ab7535
theozebua <ronizebua.rz@gmail.com>
2023-02-02 14:14:09 +0700

    `feat:` add new page and route for user profile

--- react-frontend/src/components/layouts/AppLayout.tsx
+++ react-frontend/src/components/layouts/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useContext, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { TokenContext } from '../../contexts/TokenContext'
 import Http from '../../utils/Http'
 import { ReactComponent as Logo } from '../../assets/react.svg'
@@ -10,6 +10,7 @@ type Props = {
 
 export default ({ children }: Props): JSX.Element => {
   const navigate = useNavigate()
+  const { pathname } = useLocation()
   const { token, validateToken } = useContext(TokenContext)
 
   const signOut = async (): Promise<void> => {
@@ -26,6 +27,10 @@ export default ({ children }: Props): JSX.Element => {
     }
   }
 
+  const isAuthenticationRoute = (): boolean => {
+    return pathname === '/signin' || pathname === '/signup'
+  }
+
   useEffect((): void => {
     if (!token) {
       navigate('/signin')
@@ -40,25 +45,27 @@ export default ({ children }: Props): JSX.Element => {
 
   return (
     <div className='flex h-screen flex-col'>
-      <nav className='bg-white shadow'>
-        <div className='container mx-auto p-4 md:flex md:justify-between'>
-          <div className='flex items-center justify-center gap-x-2 md:justify-start'>
-            <Logo className='animate-spin-slow' />
-            <span className='font-semibold'>Task Management System</span>
+      {!isAuthenticationRoute() && (
+        <nav className='bg-white shadow'>
+          <div className='container mx-auto p-4 md:flex md:justify-between'>
+            <div className='flex items-center justify-center gap-x-2 md:justify-start'>
+              <Logo className='animate-spin-slow' />
+              <span className='font-semibold'>Task Management System</span>
+            </div>
+            <ul className='hidden md:flex md:gap-x-8'>
+              <li className={`${pathname === '/' && 'font-semibold'}`}>
+                <Link to={'/'}>Tasks</Link>
+              </li>
+              <li className={`${pathname === '/profile' && 'font-semibold'}`}>
+                <Link to={'/profile'}>Profile</Link>
+              </li>
+              <li>
+                <button onClick={() => signOut()}>Sign Out</button>
+              </li>
+            </ul>
           </div>
-          <ul className='hidden md:flex md:gap-x-8'>
-            <li className='font-semibold'>
-              <Link to={'#'}>Tasks</Link>
-            </li>
-            <li>
-              <Link to={'#'}>Profile</Link>
-            </li>
-            <li>
-              <button onClick={() => signOut()}>Sign Out</button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        </nav>
+      )}
       <div className='container mx-auto p-4'>{children}</div>
     </div>
   )
