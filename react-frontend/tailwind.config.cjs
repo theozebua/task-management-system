41619b8e2d0efae8f3ade8b513fbb9efe8c2bd6f
theozebua <ronizebua.rz@gmail.com>
2023-02-02 13:41:12 +0700

    `feat:` add custom spin animation

--- react-frontend/tailwind.config.cjs
+++ react-frontend/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
