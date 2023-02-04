88f71cb95067b623ae7098aa83c0daeadac1126e
theozebua <ronizebua.rz@gmail.com>
2023-02-04 11:14:46 +0700

    `feat:` routing
    - Modify `Routes.php`
    - Create skeleton auth controllers such as: `LoginController.php`, `RegisterController.php`, and `LogoutControler.php`

---
+++ codeigniter4-backend/app/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers\Api\Auth;
+
+use App\Controllers\BaseController;
+
+class RegisterController extends BaseController
+{
+    public function handle()
+    {
+        //
+    }
+}
