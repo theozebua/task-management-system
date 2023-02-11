dd624c53f97a88e5b76b3de1225343d86271b8de
theozebua <ronizebua.rz@gmail.com>
2023-02-11 19:23:36 +0700

    `refactor:` add custom validation error message

--- codeigniter4-backend/app/Validation/PasswordRules.php
+++ codeigniter4-backend/app/Validation/PasswordRules.php
@@ -12,17 +12,23 @@ use function unserialize;
 
 class PasswordRules
 {
-    public function current_password(string $currentPassword): bool
+    public function current_password(string $currentPassword, ?string &$error = null): bool
     {
         /** @var PersonalAccessTokenEntity $accessToken */
         $accessToken = unserialize(request()->header('access-token')->getValue());
 
-        return password_verify(
+        if (!password_verify(
             $currentPassword,
             (new $accessToken->tokenable_type)
                 ->select(['password'])
                 ->find($accessToken->tokenable_id)
                 ->password
-        );
+        )) {
+            $error = lang('Validation.current_password', ['field' => 'Current Password']);
+
+            return false;
+        }
+
+        return true;
     }
 }
