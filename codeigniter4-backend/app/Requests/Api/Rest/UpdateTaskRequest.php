1c2365e9831cdb2b0a8ea4d952a3494f57d2bb14
theozebua <ronizebua.rz@gmail.com>
2023-02-11 15:12:18 +0700

    `feat:` create `CreateTaskRequest.php` and `UpdateTaskRequest.php`

---
+++ codeigniter4-backend/app/Requests/Api/Rest/UpdateTaskRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Requests\Api\Rest;
+
+use App\Abstracts\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    protected array $attributes = [
+        'title',
+        'description',
+        'due_date',
+        'priority',
+    ];
+
+    protected function rules(): array
+    {
+        return [
+            'title'       => ['required', 'max_length[255]'],
+            'description' => [],
+            'due_date'    => ['required', 'valid_date'],
+            'priority'    => ['required', 'integer', 'numeric', 'valid_priority'],
+        ];
+    }
+
+    protected function labels(): array
+    {
+        return [
+            'title'       => 'Title',
+            'description' => 'Description',
+            'due_date'    => 'Due Date',
+            'priority'    => 'Priority',
+        ];
+    }
+}
