d3fb41ae7f575fa4a007b6603c26b6c792dd6482
theozebua <ronizebua.rz@gmail.com>
2023-05-21 23:18:53 +0700

    `chore:` make register test

---
+++ codeigniter4-backend/tests/app/Feature/Api/Auth/RegisterTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Feature\Api\Auth\RegisterTest;
+
+use CodeIgniter\HTTP\Response;
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\DatabaseTestTrait;
+use CodeIgniter\Test\FeatureTestTrait;
+use Tests\Support\Database\Seeds\DatabaseSeeder;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+final class RegisterTest extends CIUnitTestCase
+{
+    use DatabaseTestTrait;
+    use FeatureTestTrait;
+
+    protected $seed = DatabaseSeeder::class;
+
+    private string $endpoint = '/api/register';
+
+    /**
+     * @dataProvider provideInvalidJsonData
+     */
+    public function testItReturnsValidationErrors(array $invalidJsonData): void
+    {
+        $this->withBodyFormat('json')->post($this->endpoint, $invalidJsonData)
+            ->assertStatus(Response::HTTP_BAD_REQUEST);
+    }
+
+    public function testUserRegisteredSuccessfully(): void
+    {
+        $this->withBodyFormat('json')->post($this->endpoint, [
+            'name'                  => 'Jane Doe',
+            'email'                 => 'janedoe@gmail.com',
+            'password'              => 'password',
+            'password_confirmation' => 'password',
+        ])->assertStatus(Response::HTTP_CREATED);
+    }
+
+    public function provideInvalidJsonData(): array
+    {
+        return json_decode(file_get_contents(__DIR__ . '/register.json'), true);
+    }
+}
