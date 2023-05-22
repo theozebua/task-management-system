ccc0dd75869ea9b733b4d294c0e5cc650c124219
theozebua <ronizebua.rz@gmail.com>
2023-05-22 18:42:48 +0700

    `chore:` install and setup dotenv

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,10 +1,14 @@
 import express, { Express } from 'express'
+import * as dotenv from 'dotenv'
 import route from './routes/web'
 
+dotenv.config()
+
 const app: Express = express()
-const port: number = 8000
+const baseUrl: string | undefined = process.env.BASE_URL
+const port: string | undefined = process.env.PORT
 
 app.use(route)
 app.listen(port, () => {
-  console.log(`Server run on http://localhost:${port}`)
+  console.log(`Server run on ${baseUrl}:${port}`)
 })
